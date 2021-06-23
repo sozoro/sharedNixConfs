3e4337afd0e9bc1b326b55926eb668358d315899
sozoro <46480375+sozoro@users.noreply.github.com>
2021-06-23 17:37:05 +0900

    markdown-extra-syntax

---
+++ packages/vimPlugin/markdown-extra-syntax.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+let
+  packagePath = ./markdown-extra-syntax;
+  packageName = lib.last (builtins.split "/" (toString packagePath));
+  install     = true;
+in
+  { nixpkgs.config = {
+      packageOverrides = oldpkgs: let newpkgs = oldpkgs.pkgs; in{
+        "myVimPlugins_${packageName}" = newpkgs.vimUtils.buildVimPluginFrom2Nix {
+          name         = packageName;
+          src          = packagePath;
+          dependencies = [];
+        };
+      };
+    };
+
+    programs.vim = {
+      plugins.start = if install then [ pkgs."myVimPlugins_${packageName}" ] else [];
+      extraConfig   = ''
+        augroup markdown_extra_syntax
+            au! BufNewFile,BufFilePre,BufRead *.md set filetype=markdown_extra
+        augroup END
+      '';
+    };
+  }
