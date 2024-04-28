74a202b41c0beb67658a4ee3fe3f59ed5bb9baf0
sozoro <46480375+sozoro@users.noreply.github.com>
2024-04-29 02:32:28 +0900

    flaking fonts

--- configs/fonts.nix
+++ configs/fonts.nix
@@ -1,58 +1,49 @@
-{
-  description = "font setting";
-
-  inputs = {
-  };
-
-  outputs = {
-    fonts = { config, pkgs, lib, ... }:
-      let
-        customIosevka = import ./iosevka.nix { inherit pkgs; };
-        # DPI for 2560x1440 display
-        dpi = 160;
-      in
-        { fonts = {
-            packages = with pkgs; [
-              # noto-fonts
-              # noto-fonts-cjk
-              # non-variable-noto-fonts.noto-fonts
-              # non-variable-noto-fonts.noto-fonts-cjk-sans
-              # non-variable-noto-fonts.noto-fonts-cjk-serif
-              meslo-lg
-              # customIosevka
-              mplus-outline-fonts.githubRelease
-              ibm-plex
-              # (import ./iosevkaNotoCJK.nix { inherit pkgs; })
-            ];
-            fontconfig = {
-              defaultFonts = {
-                monospace = [
-                  "Custom Iosevka"
-                  "IBM Plex Mono"
-                  "Meslo LG S"
-                  "Noto Sans Mono"
-                ];
-                serif     = lib.mkOrder 75 [ "IBM Plex Serif" "Noto Sans" ];
-                sansSerif = lib.mkOrder 75 [ "IBM Plex Sans" "Noto Sans" ];
-              };
-              allowBitmaps = false;
-              antialias    = true;
-              hinting      = {
-                enable   = true;
-                autohint = false;
-              };
-              subpixel = {
-                lcdfilter = "default";
-                rgba      = "rgb";
-              };
-              # penultimate.enable = false;
-            };
-          };
-          services.xserver.displayManager.sessionCommands = ''
-            ${pkgs.xorg.xrdb}/bin/xrdb -merge <<EOF
-               Xft.dpi: ${toString dpi}
-            EOF
-          '';
+{ config, pkgs, lib, ... }:
+let
+  customIosevka = import ./iosevka.nix { inherit pkgs; };
+  # DPI for 2560x1440 display
+  dpi = 160;
+in
+  { fonts = {
+      packages = with pkgs; [
+        # noto-fonts
+        # noto-fonts-cjk
+        non-variable-noto-fonts.noto-fonts
+        non-variable-noto-fonts.noto-fonts-cjk-sans
+        non-variable-noto-fonts.noto-fonts-cjk-serif
+        meslo-lg
+        customIosevka
+        mplus-outline-fonts.githubRelease
+        ibm-plex
+        # (import ./iosevkaNotoCJK.nix { inherit pkgs; })
+      ];
+      fontconfig = {
+        defaultFonts = {
+          monospace = [
+            "Custom Iosevka"
+            "IBM Plex Mono"
+            "Meslo LG S"
+            "Noto Sans Mono"
+          ];
+          serif     = lib.mkOrder 75 [ "IBM Plex Serif" "Noto Sans" ];
+          sansSerif = lib.mkOrder 75 [ "IBM Plex Sans" "Noto Sans" ];
         };
+        allowBitmaps = false;
+        antialias    = true;
+        hinting      = {
+          enable   = true;
+          autohint = false;
+        };
+        subpixel = {
+          lcdfilter = "default";
+          rgba      = "rgb";
+        };
+        # penultimate.enable = false;
       };
-    }
+    };
+    services.xserver.displayManager.sessionCommands = ''
+      ${pkgs.xorg.xrdb}/bin/xrdb -merge <<EOF
+         Xft.dpi: ${toString dpi}
+      EOF
+    '';
+  }
