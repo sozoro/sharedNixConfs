b2b1e222fda894d021a77c0a6a49f947ae0ccf2d
sozoro <46480375+sozoro@users.noreply.github.com>
2024-04-29 03:40:08 +0900

    fix libinput

--- configs/xserver.nix
+++ configs/xserver.nix
@@ -17,22 +17,25 @@ in
     };
     systemd.user.services.picom.wantedBy = lib.mkForce [];
 
-    services.xserver = {
-      autoRepeatDelay     = 220;
-      autoRepeatInterval  = 30;
-      exportConfiguration = true;
-      serverFlagsSection  = ''
-        Option "BlankTime" "0"
-        Option "StandbyTime" "0"
-        Option "SuspendTime" "0"
-        Option "OffTime" "0"
-      '';
-      displayManager.sessionCommands = ''
-        export TERM=xterm-256color
-        ${pkgs.xorg.xrdb}/bin/xrdb -merge ${xresources}
-        ${pkgs.xorg.xset}/bin/xset r rate 220 30
-        ${config.systemd.user.services.picom.serviceConfig.ExecStart} -b
-      '';
+    services = {
+      xserver = {
+        autoRepeatDelay     = 220;
+        autoRepeatInterval  = 30;
+        exportConfiguration = true;
+        serverFlagsSection  = ''
+          Option "BlankTime" "0"
+          Option "StandbyTime" "0"
+          Option "SuspendTime" "0"
+          Option "OffTime" "0"
+        '';
+        displayManager.sessionCommands = ''
+          export TERM=xterm-256color
+          ${pkgs.xorg.xrdb}/bin/xrdb -merge ${xresources}
+          ${pkgs.xorg.xset}/bin/xset r rate 220 30
+          ${config.systemd.user.services.picom.serviceConfig.ExecStart} -b
+        '';
+      };
+
       libinput = {
         enable = true;
       };
