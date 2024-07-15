0081877eb9892c1a1f618b25679bc2489f6c8541
sozoro <46480375+sozoro@users.noreply.github.com>
2024-07-15 13:52:25 +0900

    fix gnome

--- configs/gnome.nix
+++ configs/gnome.nix
@@ -2,5 +2,5 @@
 { programs.dconf.enable = true;
   # services.dbus.packages = [ pkgs.gnome3.dconf ];
   services.udev.packages = with pkgs; [ gnome3.gnome-settings-daemon ];
-  environment.systemPackages = with pkgs; [ gtk3 gnome3.adwaita-icon-theme ];
+  environment.systemPackages = with pkgs; [ gtk3 adwaita-icon-theme ];
 }
