be33d44afd59296ec9f01854806eb9058edded5b
sozoro <46480375+sozoro@users.noreply.github.com>
2024-12-14 19:59:47 +0900

    1214 update

--- configs/gnome.nix
+++ configs/gnome.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 { programs.dconf.enable = true;
   # services.dbus.packages = [ pkgs.gnome.dconf ];
-  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+  services.udev.packages = with pkgs; [ gnome-settings-daemon ];
   environment.systemPackages = with pkgs; [ gtk3 adwaita-icon-theme ];
 }
