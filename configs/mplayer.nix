8b6d672001ec1c9cd3309c70ea42ff17923cac2b
sozoro <46480375+sozoro@users.noreply.github.com>
2024-12-14 21:09:13 +0900

    fix mplayer 1.5 unstable

--- configs/mplayer.nix
+++ configs/mplayer.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
 { nixpkgs.config.packageOverrides = oldpkgs: let newpkgs = oldpkgs.pkgs; in {
-    mplayer = oldpkgs.mplayer.override {
+    mplayer = (oldpkgs.mplayer.override {
       jackaudioSupport = true;
-      # bluraySupport    = false;
-    };
+      bluraySupport    = false;
+    }).overrideAttrs (attrs: oldAttrs: {
+      # For ffmpeg 1.5-unstable-2024-07-03
+      # https://www.linuxquestions.org/questions/slackware-14/compile-latest-mplayer-svn-in-slackware-15-0-a-4175743148/
+      # downgrade ffmpeg version
+      buildInputs = (builtins.filter (x: ! (newpkgs.lib.hasPrefix "ffmpeg" x.name)) oldAttrs.buildInputs) ++ [ newpkgs.ffmpeg_6 ];
+    });
   };
 
   environment.systemPackages = with pkgs; [ mplayer ];
