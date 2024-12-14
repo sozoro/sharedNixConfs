9616deb315a40e45829fa04776e7d1909906b1eb
sozoro <46480375+sozoro@users.noreply.github.com>
2024-12-14 21:15:38 +0900

    fix mplayer 1.5 unstable

--- configs/mplayer.nix
+++ configs/mplayer.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 let
   # for mplayer 1.5-unstable-2024-07-03
-  patch = builtins.fetchurl {
+  patch = fetchurl {
     url = "https://gitlab.archlinux.org/archlinux/packaging/packages/mplayer/-/blob/main/ffmpeg-7.patch";
     hash = "0l80dr3c0hqpz4yadawh1k5fnr7hka2dccain1awpimxbrcjb32x";
   };
