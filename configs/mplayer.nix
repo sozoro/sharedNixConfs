3a41588db7a0d67cac0e923895ca0a47e08c9b6b
sozoro <46480375+sozoro@users.noreply.github.com>
2024-12-14 21:09:13 +0900

    fix mplayer 1.5 unstable

--- configs/mplayer.nix
+++ configs/mplayer.nix
@@ -1,9 +1,18 @@
 { pkgs, ... }:
+let
+  # for mplayer 1.5-unstable-2024-07-03
+  patch = builtins.fetchurl {
+    url = "https://gitlab.archlinux.org/archlinux/packaging/packages/mplayer/-/blob/main/ffmpeg-7.patch";
+    hash = "0l80dr3c0hqpz4yadawh1k5fnr7hka2dccain1awpimxbrcjb32x";
+  };
+in
 { nixpkgs.config.packageOverrides = oldpkgs: let newpkgs = oldpkgs.pkgs; in {
-    mplayer = oldpkgs.mplayer.override {
+    mplayer = (oldpkgs.mplayer.override {
       jackaudioSupport = true;
-      # bluraySupport    = false;
-    };
+      bluraySupport    = false;
+    }).overrideAttrs (attrs: oldAttrs: {
+      patches = oldAttrs.patches ++ [ patch ];
+    });
   };
 
   environment.systemPackages = with pkgs; [ mplayer ];
