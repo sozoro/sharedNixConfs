e059c83b96613082cff60ab2e46a1b4851a5aa9c
sozoro <46480375+sozoro@users.noreply.github.com>
2025-05-05 21:00:37 +0900

    fix mplayer

--- configs/mplayer.nix
+++ configs/mplayer.nix
@@ -20,7 +20,7 @@ in
       # For ffmpeg 1.5-unstable-2024-07-03
       # https://www.linuxquestions.org/questions/slackware-14/compile-latest-mplayer-svn-in-slackware-15-0-a-4175743148/
       # downgrade ffmpeg version
-      # buildInputs = (builtins.filter (x: ! (newpkgs.lib.hasPrefix "ffmpeg" x.name)) oldAttrs.buildInputs) ++ [ newpkgs.ffmpeg_6 ];
+      buildInputs = (builtins.filter (x: ! (newpkgs.lib.hasPrefix "ffmpeg" x.name)) oldAttrs.buildInputs) ++ [ newpkgs.ffmpeg_6 ];
     });
   };
 
