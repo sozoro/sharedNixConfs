5ada125e62cb641e565026e22d92dec2211bc002
sozoro <46480375+sozoro@users.noreply.github.com>
2025-05-05 20:45:06 +0900

    patch mplayer 1.5-unstable-2024-12-21

--- configs/mplayer.nix
+++ configs/mplayer.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
 let
   # for mplayer 1.5-unstable-2024-07-03
-  patch = fetchurl {
+  patch20240703 = fetchurl {
     url = "https://gitlab.archlinux.org/archlinux/packaging/packages/mplayer/-/blob/main/ffmpeg-7.patch";
     hash = "0l80dr3c0hqpz4yadawh1k5fnr7hka2dccain1awpimxbrcjb32x";
+  };
+  # for mplayer 1.5-unstable-2024-12-21
+  patch20241125 = fetchurl {
+    url = "https://ftp.openbsd.dk/mirrors/gentoo-portage/media-video/mplayer/files/mplayer-1.5_p20241125-c99.patch";
+    hash = "0a6vw96i7m8siiy0b5bhxbxw44rzcr0d4yccnhirmi0izqnjzg4d";
   };
 in
 { nixpkgs.config.packageOverrides = oldpkgs: let newpkgs = oldpkgs.pkgs; in {
@@ -11,7 +16,7 @@ in
       jackaudioSupport = true;
       bluraySupport    = false;
     }).overrideAttrs (attrs: oldAttrs: {
-      patches = (oldAttrs.patches or []) ++ [ patch ];
+      patches = (oldAttrs.patches or []) ++ [ patch20241125 ];
     });
   };
 
