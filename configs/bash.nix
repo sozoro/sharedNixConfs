9366e3e7fab42418dbe087696660a93124e94503
sozoro <46480375+sozoro@users.noreply.github.com>
2025-12-31 16:30:01 +0900

    set completion-ignore-case

--- configs/bash.nix
+++ configs/bash.nix
@@ -12,6 +12,7 @@
       export HISTSIZE=10000
       bind '"\C-p": history-search-backward'
       bind '"\C-n": history-search-forward'
+      bind 'set completion-ignore-case on'
     '';
 
     promptInit = ''
