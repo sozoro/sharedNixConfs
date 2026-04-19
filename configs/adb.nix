59ac1222aed8859886e15292b4f1d9b54acab1aa
sozoro <46480375+sozoro@users.noreply.github.com>
2026-04-19 19:57:41 +0900

    use android-tools

--- configs/adb.nix
+++ configs/adb.nix
@@ -1,11 +1,7 @@
 { config, pkgs, lib, ... }:
 { config = {
-    programs.adb.enable = true;
-    # users.users.<your-user>.extraGroups = [ "adbusers" ];
-
-    # 'android-udev-rules' has been removed due to being superseded by built-in systemd uaccess rules.
-    #services.udev.packages = [
-    #  pkgs.android-udev-rules
-    #];
+    environment.systemPackages = with pkgs; [
+      android-tools
+    ];
   };
 }
